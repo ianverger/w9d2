38932c3226cbf2e30055cb011c449cbf35a1c356
acoop68@gmail.com <acoop68@gmail.com>
2022-09-13 17:05:35 -0400

    complete tic tac toe game

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -33,15 +33,27 @@ class View {
   handleClick(e) {
       if (e.target.nodeName === "LI") {
         let pos = e.target.id
+        // e.target.class = "played"
         this.makeMove(pos)
       }
     }
     
-  makeMove(pos) {
-    const ele = document.getElementById(pos)
-    ele.class = "played"
-    
-    this.game.playMove(pos)
+    makeMove(pos) {
+      const ele = document.getElementById(pos)
+      // ele.setAttribute("class", "played")
+      let mark = `${this.game.currentPlayer}`
+      const position = [parseInt(pos[0]), parseInt(pos[2])]
+      if (ele.className !== 'x' && ele.className !== 'o') {
+        ele.setAttribute("class", mark)
+        let text = document.createTextNode(mark.toUpperCase())
+        ele.append(text)
+        this.game.playMove(position)
+      } else {
+        alert("not a valid move!")
+      }
+      if (this.game.isOver()) {
+        alert(`Congrats! ${mark} has won!!!`)
+      }
   }
 
 }
